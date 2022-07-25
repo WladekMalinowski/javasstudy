4c5c97e5de96fb7fbb93022ac62dad149a88baac
Wladek <malinowskiwladek@gmail.com>
2022-07-25 13:01:32 +0200

    third

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-"use strict";
+"use strickt"
 
 const numberOfFilms = +prompt('How many films did you watch?', '');
 
@@ -6,19 +6,16 @@ const personalMovieBD = {
     count: numberOfFilms,
     movies: {},
     actors: {},
-    genres: [],
+    geners: [],
     privat: false
 };
 
-const a = prompt ('What did you watch the last film?', ''),
-      b = prompt ('Please rate this film', ''),
-      c = prompt ('What did you watch the last film?', ''),
-      d = prompt ('Please rate this film', '');
+const a = prompt('What the last film did youu see?' , '');
+      b = prompt('Please rate it', '');
+      c = prompt('What the last film did youu see?' , '');
+      d = prompt('Please rate it', '');
 
 personalMovieBD.movies[a] = b;
 personalMovieBD.movies[c] = d;
 
 console.log(personalMovieBD);
-
-
-
