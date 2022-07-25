784d64dbe9ae674b9d43bfb0bd9425164c590f3c
Wladek <malinowskiwladek@gmail.com>
2022-07-25 13:43:08 +0200

    js update

--- js/script.js
+++ js/script.js
@@ -1,21 +1,32 @@
 "use strickt"
 
-const numberOfFilms = +prompt('How many films did you watch?', '');
+// const numberOfFilms = +prompt('How many films did you watch?', '');
 
-const personalMovieBD = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    geners: [],
-    privat: false
-};
+// const personalMovieBD = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     geners: [],
+//     privat: false
+// };
 
-const a = prompt('What the last film did youu see?' , '');
-      b = prompt('Please rate it', '');
-      c = prompt('What the last film did youu see?' , '');
-      d = prompt('Please rate it', '');
+// const a = prompt('What the last film did youu see?' , '');
+//       b = prompt('Please rate it', '');
+//       c = prompt('What the last film did youu see?' , '');
+//       d = prompt('Please rate it', '');
 
-personalMovieBD.movies[a] = b;
-personalMovieBD.movies[c] = d;
+// personalMovieBD.movies[a] = b;
+// personalMovieBD.movies[c] = d;
 
-console.log(personalMovieBD);
+// console.log(personalMovieBD);
+
+
+const coffee = 6,
+      sandwich = 2,
+      juice = 1;
+
+if (coffee === 6 && juice === 1) {
+    console.log('Go away!');
+} else {
+    console.log('Good!');
+}
